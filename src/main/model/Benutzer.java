217d502b2517b0b4b4399ddf3d76b2c28da5a9be
D073973 <cora.hartmann@sap.com>
2020-10-12 11:46:05 +0200

    Klasse Benutzer erstellt

---
+++ src/main/model/Benutzer.java
@@ -0,0 +1,38 @@
+package main.model;
+
+public class Benutzer {
+    private String vorname;
+    private String nachname;
+    private EMail email;
+
+    // Konstruktor zum Erstellen des Benutzers aus der csv-Datei
+    public Benutzer(String vorname, String nachname, EMail email) {
+        this.vorname = vorname;
+        this.nachname = nachname;
+        this.email = email;
+    }
+
+    public String getVorname() {
+        return vorname;
+    }
+
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
+    }
+
+    public String getNachname() {
+        return nachname;
+    }
+
+    public void setNachname(String nachname) {
+        this.nachname = nachname;
+    }
+
+    public EMail getEmail() {
+        return email;
+    }
+
+    public void setEmail(EMail email) {
+        this.email = email;
+    }
+}
